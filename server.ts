cc76c22c3d8f098237be3e1a7a99185a835cebea
Prakhar Varshney <prakharnegative@gmail.com>
2020-05-18 19:36:45 +0530

    Create server.ts

---
+++ server.ts
@@ -0,0 +1,10 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+const server = serve({ port: 3000 });
+
+
+console.log("http://localhost:3000/");
+
+
+for await (const req of server) {
+  req.respond({ body: "Hello World\n" });
+}
